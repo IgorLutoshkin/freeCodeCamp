58b0797c0e3a14e03994406f193916f5d05270bb
IgorLutoshkin <100241812+IgorLutoshkin@users.noreply.github.com>
2022-08-03 02:24:33 +0300

    Update 53.js

--- 53.js
+++ 53.js
@@ -1,10 +1,11 @@
-function countup(n) {
-    if (n < 1) {
-      return [];
-    } else {
-      const countArray = countup(n - 1);
-      countArray.push(n);
-      return countArray;
-    }
+// Only change code below this line
+function countdown(n){
+  if (n < 1) {
+    return [];
+  } else {
+    const Array = countdown(n - 1);
+    Array.unshift(n);
+    return Array;
   }
-  console.log(countup(5));
+}
+// Only change code above this line
